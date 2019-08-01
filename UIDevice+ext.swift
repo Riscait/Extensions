f38e756022383bfea1d8748e2d360172f8f31791
Ryunosuke Muramatsu <mr@nerco.jp>
2019-08-01 11:41:35 +0900

    Create UIDevice+ext.swift

---
+++ UIDevice+ext.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension UIDevice {
+    
+    // デバイスの型名を取得する
+    static let modelName: String = {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        return machineMirror.children.reduce("") { id, element in
+            guard let value = element.value as? Int8, value != 0 else { return id }
+            return id + String(UnicodeScalar(UInt8(value)))
+        }
+    }()
+}
